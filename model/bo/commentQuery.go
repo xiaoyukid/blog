5dfe3eae99cb46f0ec6715e2ff5d1305cb5835e1
Fisher <chao.yu@palmpay-inc.com>
2025-08-06 13:58:57 +0800

    comment handler

--- model/bo/commentQuery.go
+++ model/bo/commentQuery.go
@@ -4,6 +4,6 @@ type CommentQuery struct {
 	PostId         uint   `query:"postId" binding:"required"`
 	Page           int    `query:"page" binding:"required"`
 	Size           int    `query:"size" binding:"required"`
-	UserId         int    `query:"userId" binding:"required"`
+	UserId         uint   `query:"userId"`
 	CommentContent string `json:"commentContent"`
 }
--- web/commentHandler.go
+++ web/commentHandler.go
@@ -1,6 +1,13 @@
 package web
 
-import "blog/service"
+import (
+	"blog/common/constModel"
+	models "blog/model"
+	"blog/model/bo"
+	"blog/service"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 type CommentHandler struct {
 	commentService service.CommentService
@@ -8,4 +15,33 @@ type CommentHandler struct {
 
 func NewCommentHandler(commentService service.CommentService) *CommentHandler {
 	return &CommentHandler{commentService: commentService}
+}
+
+func (c *CommentHandler) GetComments(context *gin.Context) {
+	var query bo.CommentQuery
+	if err := context.ShouldBind(&query); err != nil {
+		context.Error(constModel.ErrAuthInvalid.WithDetails("请输入分页参数： %v" + err.Error()))
+		return
+	}
+	userID := context.GetFloat64("userId")
+	if userID == 0 {
+		context.Error(constModel.ErrAuthInvalid.WithDetails("请登录后操作！"))
+		return
+	}
+	commentResult := c.commentService.GetComments(query)
+	context.JSON(http.StatusOK, commentResult)
+}
+
+func (c *CommentHandler) CreateComment(context *gin.Context) {
+	var comment models.Comment
+
+	userID := context.GetUint("userId")
+	comment.UserID = userID
+
+	createComment, err := c.commentService.CreateComment(&comment)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, createComment)
 }
